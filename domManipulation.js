66ae7961f094582bc9c515c35cbe72ab90ed0f25
OsbornDelAngel <ozdelangel@gmail.com>
2022-07-30 23:28:45 -0700

    made the quiz page and some perliinary javascript to get it started. the question container also has styles along with hide functionality.

--- domManipulation.js
+++ domManipulation.js
@@ -1,7 +1,40 @@
-const arr = [1,2,3,4];
+// first we need to start the game by using dom manipulation to grab the id
 
-const [first, second] = arr;
+const startButton = document.getElementById('start-btn');
 
-console.log(first, second);
-console.log(arr);
+// then we are going to add an event when the button is clicked
+
+startButton.addEventListener('click', startGame);
+
+// this is how we start the game with a function
+
+// grab the question container
+const questionContainer = document.getElementById('question-container');
+
+function startGame() {
+    console.log('clicked');
+    startButton.classList.add('hide');
+    questionContainer.classList.remove('hide');
+    //we need to make a callback to the set next question function to get the next questions
+    setNextQuestion()
+
+}
+
+// this function will get the next question
+function setNextQuestion() {
+
+}
+
+// we need to build our questions with objects and arrays
+const questions = [
+    
+
+]
+
+
+
+// how you start any project is just remember the workflow.
+// we need to start game
+// get next question
+// select answer
 
